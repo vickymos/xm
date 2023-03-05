92b147aa0fe64eac551e71193e3ceeb6075922df
v.moschonisiotou <v.moschonisiotou@wedia.gr>
2023-03-05 12:01:18 +0200

    removing token from storage on signout

--- src/components/parts/authStatus.tsx
+++ src/components/parts/authStatus.tsx
@@ -1,29 +1,37 @@
-import {
-    useNavigate,
-    Link
-} from "react-router-dom";
-
+import { useNavigate, Link } from "react-router-dom";
 import { useAuth } from "../../App";
 import "./authStatus.css";
 
 const AuthStatus = () => {
 
     let auth = useAuth();
+
     let navigate = useNavigate();
 
+    const handleSignout = () => {
+        auth.signout(() => navigate("/"));
+        if (typeof window !== "undefined")
+            window.localStorage.removeItem("xmTokenLocalKey");
+    }
+
     if (!auth.user) {
-        return <div className="auth-actions">
-            <Link to="/login" className="button button--primary mb-16">Login</Link>
-            <div>so you can create a delicious burger</div>
-        </div>;
+        return (
+            <div className="auth-actions">
+
+                <Link to="/login" className="button button--primary mb-16">Login</Link>
+
+                <div>so you can create a delicious burger</div>
+
+            </div>
+        );
     }
 
     return (
         <div className="auth-actions">
 
             <div className="mb-16">Welcome {auth.user}!{" "}</div>
-            
-            <button className="button button--primary" onClick={() => { auth.signout(() => navigate("/")); }}>
+
+            <button className="button button--primary" onClick={() => { handleSignout() }}>
                 Sign out
             </button>
 
